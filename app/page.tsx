2ec7cd77805e738d7fe8e8f107d52686b5b3e713
george1994grandmaster <gkiladze1994@gmail.com>
2023-11-06 18:21:47 +0400

    fffff

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       
-      <img src="https://i.sozcucdn.com/wp-content/uploads/2019/06/15/iecrop/ferrari-sf90-stradale-1_16_9_1560591735.jpg?w=900&h=900&mode=crop&scale=both" alt="" />
+      <div>hi</div>
     </main>
   )
 }
